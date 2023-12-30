5ed38d8e9e29ddff0c0d1ade1547ba175967d1a0
JNethaduna <147524613+JNethaduna@users.noreply.github.com>
2023-12-30 22:10:18 +0530

    Fixed small issue with feedback submission

--- src/main/java/com/green/supermarketwebapp/Controllers/FeedbackController.java
+++ src/main/java/com/green/supermarketwebapp/Controllers/FeedbackController.java
@@ -4,7 +4,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.green.supermarketwebapp.services.FeedbackService;
@@ -30,7 +29,7 @@ public class FeedbackController {
   }
 
   @PostMapping("/user/feedback/submit")
-  public String submitFeedback(@RequestBody String feedback) {
+  public String submitFeedback(@RequestParam("feedback") String feedback) {
     feedbackService.saveFeedback(feedback);
     return "redirect:/feedback-success";
   }
